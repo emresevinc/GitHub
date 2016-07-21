dd56b8e7811cd1d787da2071326f46856a639288
Emre Sevinç <emresevinc10@gmail.com>
2016-07-21 18:26:57 +0300

    düzenleme

--- PictureExample1/src/AppWindow1.java
+++ PictureExample1/src/AppWindow1.java
@@ -7,36 +7,30 @@ import java.util.List;
 
 import javax.imageio.ImageIO;
 
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ExpandBar;
 import org.eclipse.swt.widgets.ExpandItem;
 import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.dnd.DragSource;
-import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import bean.CoreTemplate;
 import bean.Model;
 import bean.Product;
-
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.graphics.Point;
 
 public class AppWindow1 {
 
@@ -85,7 +79,13 @@ public class AppWindow1 {
 	Button rdBtnSablon2 = null;
 	Button rdBtnSablon3 = null;
 	private static final String logoAppPath = "C:/LogoApp/Models";
-	List<Label> modelList = null;
+	private static final String scaledLogoPath = "C:\\LogoApp\\ScaledLogos";
+	private static final String scaledAndProgressedPath = "C:\\LogoApp\\ScaledAndProgressed"; // Bu path iþlenmiþ ürünleri scale edilmiþ hallerinin bulunduðu path'tir. Ürünler iþlendikten sonra ekranda scale halini buradan alýyoruz
+	private static final String nonProgressedScaledProductPath = "C:\\LogoApp\\ScaledImages"; // Bu path ekran ilk açýldýðýnda default olan ürünlerin scale hallerinin tutulduðu path'i gösterir. Ekran ilk açýldýðýnda bu bos urunler gozlemlenir
+	private List<Model> modelList = null;
+	private CoreTemplate coreTemplate = null;
+	private Text textX;
+	private Text textY;
 	
 
 	/**
@@ -97,12 +97,6 @@ public class AppWindow1 {
 		shell.setSize(1354, 1012);
 		shell.setText("SWT Application");
 		shell.setLayout(null);
-
-		String sourcePathImg = "C:/test/18500xIG0.jpg";
-		String sourcePathLogo = "C:/test/devoicon.png";
-		String destPathImg = "C:/test/18500xIG0.png";
-		String destPathLogo = "C:/test/devoicon1.png";
-		String createPath = "C:/test/18500xIG0xx.png";
 
 		
 		//drawIt(sourcePathImg, sourcePathLogo, destPathImg, destPathLogo, createPath, 250, 300, 70, 70,oldLogoCoordinates[0],oldLogoCoordinates[1]);
@@ -124,14 +118,11 @@ public class AppWindow1 {
 		xpndtmNewExpanditem.setExpanded(true);
 		xpndtmNewExpanditem.setText("Gildan 500");
 		
-		Image imgx1 = new Image(display,"C:/test/18500xIG0xx1.png");
-		Image imgx2 = new Image(display,"C:/test/18500xIG0xx2.png");
-		Image imgx3 = new Image(display,"C:/test/18500xIG0xx3.png");
 		
 		
 		// Bu kod bloðu model ürün hiyerarþisini saðlýyor ve model ürün bilgisini elde ediyor
-		List<Model> modelList = new ArrayList<Model>();
-		CoreTemplate coreTemplate = new CoreTemplate(composite, SWT.NONE);
+		modelList = new ArrayList<Model>();// Orjinal model bilgilerini alýyor
+		coreTemplate = new CoreTemplate(composite, SWT.NONE);
 		File folder = new File(logoAppPath);
 		File[] listOfFiles = folder.listFiles();
 		Model tempModel = null;
@@ -159,67 +150,12 @@ public class AppWindow1 {
 	    }
 	    ////////////////////////////////////////////
 	    
+	    // Not : Ürünlerin iþlenmemiþ hallerinin ölçeklenmiþ halini program açýlýrken scale edip bir yere kaydederek guc harcamak yerine
+	    // C:/LogoApp/ScaledImages altýna direk küçük hale scale edilmis halini koymak is yukunu hafifletir.
 	    
-	    for(int x = 0; x < modelList.get(0).getProductList().size(); x++){
-	    	BufferedImage originalImage = null;
-	    	String originalImagePath = modelList.get(0).getProductList().get(x).getProductFullPath();
-	    	String scaledImageParentPath = "C:\\LogoApp\\ScaledImages";
-	    	String scaledImageName = modelList.get(0).getProductList().get(x).getProductName()+"-scaled.png";
-	    	try {
-				originalImage = ImageIO.read(new File(originalImagePath));
-				coreTemplate.getAllRegionList().get(x).setImage(scaleToImage(originalImage, 250, 300, scaledImageParentPath+"\\"+scaledImageName));
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-	    	
-	    }
-	    
-	    
-//		Label lblProduct1 = new Label(composite, SWT.NONE);
-//		lblProduct1.setImage(imgx1);
-//		
-//		
-//		Label lblProduct2 = new Label(composite, SWT.NONE);
-//		lblProduct2.setImage(imgx2);
-//		
-//		Label lblProduct3 = new Label(composite, SWT.NONE);
-//		lblProduct3.setText("         3");
-//		
-//		Label lblProduct4 = new Label(composite, SWT.NONE);
-//		lblProduct4.setText("        4");
-//		
-//		
-//		Label lblProduct5 = new Label(composite, SWT.NONE);
-//		//formToolkit.adapt(lblProduct5, true, true);
-//		lblProduct5.setImage(imgx3);
-//		
-//		Label lblProduct6 = new Label(composite, SWT.NONE);
-//		lblProduct6.setText("          6");
-//		
-//		Label lblProduct7 = new Label(composite, SWT.NONE);
-//		lblProduct7.setText("        7");
-//		
-//		Label lblProduct8 = new Label(composite, SWT.NONE);
-//		lblProduct8.setText("        8");
-//		
-//		Label lblProduct9 = new Label(composite, SWT.NONE);
-//		lblProduct9.setText("        9");
-//		
-//		Label lblProduct10 = new Label(composite, SWT.NONE);
-//		lblProduct10.setText("       10");
-//		
-//		Label lblProduct11 = new Label(composite, SWT.NONE);
-//		lblProduct11.setText("       11");
-//		
-//		Label lblProduct12 = new Label(composite, SWT.NONE);
-//		lblProduct12.setText("      12");
-//		
-//		Label lblProduct13 = new Label(composite, SWT.NONE);
-//		lblProduct13.setText("       13");
-//		
-//		Label lblProduct14 = new Label(composite, SWT.NONE);
-//		lblProduct14.setText("      14");
-		
+
+	    showAllProducts(nonProgressedScaledProductPath);
+
 		xpndtmNewExpanditem.setControl(composite);
 		xpndtmNewExpanditem.setHeight(700);
 		
@@ -238,7 +174,17 @@ public class AppWindow1 {
 		text.setBounds(27, 90, 257, 20);
 		
 		logoLabel = formToolkit.createLabel(shell, "", SWT.NONE);
-		logoLabel.setBounds(690, 37, 115, 115);
+		
+		//Coordinater
+		coreTemplate.getRegion3().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				textX.setText(String.valueOf(e.x));
+				textY.setText(String.valueOf(e.y));
+			}
+		});
+		//
+		logoLabel.setBounds(705, 46, 115, 115);
 		
 		Label labelTemplate1 = new Label(shell, SWT.NONE);
 		labelTemplate1.setImage(SWTResourceManager.getImage("C:\\LogoApp\\Templates\\Template1.jpg"));
@@ -289,6 +235,26 @@ public class AppWindow1 {
 		formToolkit.adapt(rdBtnSablon3, true, true);
 		rdBtnSablon3.setText("Sablon 3");
 		
+		textX = new Text(shell, SWT.BORDER);
+		textX.setBounds(415, 10, 78, 26);
+		formToolkit.adapt(textX, true, true);
+		
+		textY = new Text(shell, SWT.BORDER);
+		textY.setBounds(523, 10, 78, 26);
+		formToolkit.adapt(textY, true, true);
+		
+		Button btnApplyLogo = new Button(shell, SWT.NONE);
+		btnApplyLogo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				applyToLogo();
+			}
+		});
+		btnApplyLogo.setBounds(1672, 122, 115, 30);
+		formToolkit.adapt(btnApplyLogo, true, true);
+		btnApplyLogo.setText("Logoyu Uygula");
+		
+		
 		rdBtnSablon3.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -315,16 +281,20 @@ public class AppWindow1 {
         return imgx;
 	}
 	
-	public void drawIt(String sourcePathImg,String sourcePathLogo,String destPathImg,String destPathLogo,String createPath,int widthScaleImg,int heigthScaleImg,int widthScaleLogo,int heigthScaleLogo,int logoCoordinatX,int logoCoordinatY){
+	
+	
+	public void drawIt(String sourcePathImg,String sourcePathLogo,String destPathImg,String destPathLogo,String createPath,int widthScaleImg,int heigthScaleImg,int widthScaleLogo,int heigthScaleLogo,int logoCoordinatX,int logoCoordinatY,boolean withoutScale){
 		BufferedImage image = null;
 		BufferedImage logo = null;
 		try {
 			   image = ImageIO.read(new File(sourcePathImg));
 			   logo = ImageIO.read(new File(sourcePathLogo));
-			   scaleToImage(image, widthScaleImg, heigthScaleImg, destPathImg); // Ölçekleme yapýlýyor
+			   if(!withoutScale){// ölçeklenecekse
+				   	scaleToImage(image, widthScaleImg, heigthScaleImg, destPathImg); // Ölçekleme yapýlýyor
+				   	
+			   }
 			   scaleToImage(logo, widthScaleLogo, heigthScaleLogo, destPathLogo);
-			   
-			   image = ImageIO.read(new File(destPathImg)); // Ölçeklenmiþ kaynak dosyalara eriþiliyor.
+			   //image = ImageIO.read(new File(destPathImg)); // Ölçeklenmiþ kaynak dosyalara eriþiliyor.
 			   logo = ImageIO.read(new File(destPathLogo));
 
 			   Graphics2D imageGraphic = (Graphics2D) image.getGraphics();//createGraphics();
@@ -350,16 +320,16 @@ public class AppWindow1 {
         fd.setFilterExtensions(filterExt);
         logoPath = fd.open();
         text.setText(logoPath);
-        showLogo();
+        processLogo();
 	}
 	
-	public void showLogo(){
+	public void processLogo(){
 		BufferedImage choosenLogo = null;
 		String createPath = "C:\\LogoApp\\SmallLogos\\"+getFileName(logoPath)+"-sm.png";
 		try {
 			choosenLogo = ImageIO.read(new File(logoPath));
-			scaleToImage(choosenLogo, 115, 115, createPath);
-			dressToImg(createPath, logoLabel);
+			scaleToImage(choosenLogo, 115, 115, createPath); //C:/LogoApp/SmallLogos altýna ilgili logoyu sonunda -sm olacak þekilde ölçeklendirilmiþ halde kaydediyor
+			dressToImg(createPath, logoLabel); // Ekranda küçük logo önizlemesi için kullanýlýyor. 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -392,4 +362,119 @@ public class AppWindow1 {
 			break;
 		}
 	}
+	
+	private void applyToLogo(){
+		int height=0, width = 0,coordinatX=0,coordinatY=0;
+		switch (selectedTemplate) {
+		case 1:
+			height = 500;
+			width = 600;
+			coordinatX = 660;
+			coordinatY = 600;
+			break;
+		case 2:
+			height = 150;
+			width = 70;
+			coordinatX = 88;
+			coordinatY = 93;
+			break;
+		case 3:
+			height = 50;
+			width = 60;
+			coordinatX = 95;
+			coordinatY = 132;
+			break;
+		default:
+			break;
+		}
+		DirectoryDialog dlg = new DirectoryDialog(shell);
+		dlg.setFilterPath("C:/");
+		dlg.setText("SWT's DirectoryDialog");
+		dlg.setMessage("Select a directory");
+		String createPath = dlg.open();
+		Model tempModel = null;
+		for (int i = 0; i < modelList.size(); i++) {
+			tempModel = modelList.get(i);
+			List<Product> productList = tempModel.getProductList();
+			Product tempProduct = null;
+			for(int j = 0; j < productList.size();j++){
+				tempProduct = productList.get(j);
+				scaleLogoForOriginalProduct(tempProduct.getProductFullPath(), logoPath, getFileName(logoPath), 5); 
+				
+				drawww(tempProduct.getProductFullPath(), scaledLogoPath+"\\"+getFileName(logoPath)+".png", createPath, coordinatX, coordinatY);
+				
+				//drawIt(tempProduct.getProductFullPath(), "", "", logoPath, createPath+"\\"+tempProduct.getProductName(), 0, 0, width, height, coordinatX, coordinatY, true);
+			}
+		}
+		showAllProducts(createPath);
+	}
+	
+	// Logoyu ürünü içeren resim ile istenilen oranda küçültür
+	// Yalnýzca logo üzerinde iþlem yapmak yeterli olacaktýr
+	// Ekrana basýlan ölçeklenmiþ logolu ürünlerin buradan oluþturulacak logonun ürünün üzerine draw edilmiþ halinin ölçeklendirilmiþ hali olduðu bilinmelidir
+	// Ölceklenmis logo C:\LogoApp\ScaledLogos altýna sonunda -scaled.png/jpg olacak þekilde kaydedilecektir
+	private void scaleLogoForOriginalProduct(String productOriginalPath, String logoOriginalPath,String logoNameForPath,int scaleMeasure){ 
+		
+		BufferedImage productOriginal = null;
+		BufferedImage logoOriginal = null;
+		//düzenlenecek
+		try
+		{
+			productOriginal = ImageIO.read(new File(productOriginalPath));
+			logoOriginal = ImageIO.read(new File(logoOriginalPath));
+			int productOriginalWidth = productOriginal.getWidth();
+			int productOriginalHeight = productOriginal.getHeight();
+	//		int logoOriginalWidth = logoOriginal.getWidth();
+	//		int logoOriginalHeight = logoOriginal.getHeight();
+			
+			int createdLogoWidth = productOriginalWidth / scaleMeasure;
+			int createdLogoHeight = productOriginalHeight / scaleMeasure;
+			
+			BufferedImage resizedLogo = new BufferedImage(createdLogoWidth, createdLogoHeight, logoOriginal.getType());
+			Graphics2D graphic = resizedLogo.createGraphics();
+			graphic.drawImage(logoOriginal, 0, 0, createdLogoWidth, createdLogoHeight, null);
+			graphic.dispose();
+		
+		
+			ImageIO.write(resizedLogo, "png", new File(scaledLogoPath+"\\"+logoNameForPath+".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void drawww(String productPath,String logoPath,String createPath,int coordinatX,int coordinatY){
+		BufferedImage productBuff = null;
+		BufferedImage logoBuff = null;
+		
+		try {
+			productBuff = ImageIO.read(new File(productPath));
+			logoBuff = ImageIO.read(new File(logoPath));
+			
+			Graphics2D graphicProduct = (Graphics2D) productBuff.getGraphics();
+			graphicProduct.drawImage(logoBuff, coordinatX, coordinatY, null);
+			graphicProduct.dispose();
+			
+			ImageIO.write(productBuff, "png", new File(createPath+"\\"+getFileName(productPath)+".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void showAllProducts(String parentPath){
+		BufferedImage originalImage = null;
+		for(int x = 0; x < modelList.get(0).getProductList().size(); x++){
+	    	String productName = modelList.get(0).getProductList().get(x).getProductName();
+	    	String productNameStr = productName.substring(0, productName.length()-4)+".png";
+	    	try {
+				originalImage = ImageIO.read(new File(parentPath+"\\"+productNameStr));
+				coreTemplate.getAllRegionList().get(x).setImage(scaleToImage(originalImage, 250, 300, scaledAndProgressedPath+"\\"+productNameStr));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+	    	
+	    }
+	}
+	
 }
